296607a28cea9d93606c5b8ec6430813fc9f750e
Lucas Max Batista Hartmann <lucasmbh01@gmail.com>
2026-04-24 19:40:09 -0300

    Exemplo de erro

---
+++ src/Basico/NullPointerExececao.java
@@ -0,0 +1,10 @@
+package Basico;
+
+import POO.Basico.Exemplo1.Carro;
+
+public class NullPointerExececao {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.ligar();
+    }
+    }
