26bd6075ee8f23daca87a02b4d06781e32391ed8
Lucas Max Batista Hartmann <lucasmbh01@gmail.com>
2026-04-24 20:53:17 -0300

    Exercícios de encapsulamento

---
+++ src/POO/Basico/Encapsulamento/exemplo1/Principal.java
@@ -0,0 +1,11 @@
+package POO.Basico.Encapsulamento.exemplo1;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.setNome(null);
+        pessoa.setNome("");
+        //pessoa.apresentar();
+    }
+}
