5bdcc2e542461c0a78463c8951e7cb17d57d15b6
Lucas Max Batista Hartmann <lucasmbh01@gmail.com>
2026-04-24 19:39:46 -0300

    Exemplo de Exceção

---
+++ src/Basico/Excecao.java
@@ -0,0 +1,33 @@
+package Basico;
+
+import java.util.Scanner;
+
+public class Excecao {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1, num2;
+
+        System.out.println("Digite um número: ");
+        num1 = scanner.nextInt();
+
+        System.out.println("Digite outro número: ");
+        num2 = scanner.nextInt();
+
+        try {
+            double divisão = num1 / num2;
+            System.out.println(divisão);
+        } catch (ArithmeticException exception) {
+            System.out.println("Não pode dividir por zero bobão");
+        }
+
+        // OUTRA FORMA (Faz a mesma coisa, de uma forma diferente)
+
+        if (num2 == 0) {
+            System.out.println("Não pode dividir por zero, bobão");
+        } else {
+            System.out.println(num1 / num2);
+        }
+    }
+}
